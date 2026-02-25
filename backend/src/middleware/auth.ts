567cb19d369902905b14cec4f6508713187dade3
vanss <vans.s3046@gmail.com>
2026-02-26 00:24:02 +0530

    authorization using jwt

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,40 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  user?: {
+    id: string;
+  };
+}
+
+export const verifyToken = (req: AuthRequest,res: Response,next: NextFunction) => {
+  const authHeader = req.headers.authorization ;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT secret not defined");
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET_KEY
+    ) as JwtPayload;
+
+    req.user = {
+      id: decoded.id
+    };
+
+    console.log("middleware check done");
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
--- backend/src/routes/memories.ts
+++ backend/src/routes/memories.ts
@@ -1,8 +1,13 @@
 import {Router} from "express";
+<<<<<<< HEAD
 import { createMemory, getAllMemories } from "../controllers/memories.js";
+=======
+import { createMemory,getAllMemories } from "../controllers/memories.js";
+>>>>>>> efdf266 (authorization using jwt)
 const router = Router();
+import { verifyToken } from "../middleware/auth.js";
 
-router.route('/').post(createMemory);
-router.route('/').get(getAllMemories);
+router.route('/').post(verifyToken, createMemory);
+router.route('/').get(verifyToken, getAllMemories);
 
 export default router;
