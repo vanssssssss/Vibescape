a114b76a94e364e2d9d281272adf1bc02a84e720
vanss <vans.s3046@gmail.com>
2026-02-26 08:45:15 +0530

    memories page backend api

--- backend/src/routes/memories.ts
+++ backend/src/routes/memories.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
-<<<<<<< HEAD
-import { createMemory, getAllMemories } from "../controllers/memories.js";
-=======
-import { createMemory,getAllMemories } from "../controllers/memories.js";
->>>>>>> efdf266 (authorization using jwt)
-const router = Router();
+import { createMemory,getAllMemories,addImage,addNotes } from "../controllers/memories.js";
+import { imagekitAuth } from "../controllers/memories.js";
 import { verifyToken } from "../middleware/auth.js";
+
+const router = Router();
 
 router.route('/').post(verifyToken, createMemory);
 router.route('/').get(verifyToken, getAllMemories);
+router.patch("/:memoryId/images", verifyToken, addImage);
+router.patch("/:memoryId", verifyToken, addNotes);
+router.route('/imagekit-auth').get(imagekitAuth);
 
 export default router;
