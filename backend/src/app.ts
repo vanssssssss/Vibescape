6a2491966bc7c33175bcf9f83d03a18fb558626b
vanss <vans.s3046@gmail.com>
2026-03-29 00:52:45 +0530

    get autocomplete suggestion backen api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import authRouter from "./routes/auth.js";
 import favoritesRouter from "./routes/favorites.js";
 import memoriesRouter from "./routes/memories.js";
 import userInfoRouter from "./routes/userInfo.js";
+import geocodeRouter from "./routes/geocode.js";
 import cors from "cors";
 import "dotenv/config";
 import { initTagVectors } from "./nlp/tagVectors.js";
@@ -19,7 +20,7 @@ app.use(cors({origin:frontend_url}));
 
 app.use(express.json());
 await initTagVectors();
-
+app.set("trust proxy", 1);
 
 
 app.use('/api/v1/search',searchPlaceRouter);
@@ -27,6 +28,7 @@ app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/memories',memoriesRouter);
 app.use('/api/v1/favorites',favoritesRouter);
 app.use('/api/v1/users/me',userInfoRouter);
+app.use('/api/v1/geocode', geocodeRouter);
 app.get('/',(req, res) => { res.send("API running")});
 
 export default app;
