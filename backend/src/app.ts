b87cee6fc2717058b245c26c12178f719b849efc
vanss <vans.s3046@gmail.com>
2026-03-30 08:43:03 +0530

    savedPlaces api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import searchPlaceRouter from "./routes/searchPlace.js";
 import authRouter from "./routes/auth.js";
-import favoritesRouter from "./routes/favorites.js";
+import savedPlacesRouter from "./routes/savedPlaces.js";
 import memoriesRouter from "./routes/memories.js";
 import userInfoRouter from "./routes/userInfo.js";
 import geocodeRouter from "./routes/geocode.js";
@@ -28,7 +28,7 @@ app.use('/api/v1/search',searchPlaceRouter);
 app.use('/api/v1/rating', ratingRouter);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/memories',memoriesRouter);
-app.use('/api/v1/favorites',favoritesRouter);
+app.use('/api/v1/saved-places',savedPlacesRouter);
 app.use('/api/v1/users/me',userInfoRouter);
 app.use('/api/v1/geocode', geocodeRouter);
 app.get('/',(req, res) => { res.send("API running")});
