5c30b5ee0b54aabb63015fb4daa9e548bb70af99
vanss <vans.s3046@gmail.com>
2026-04-04 11:10:03 +0530

    admin backend api done

--- backend/src/controllers/admin.ts
+++ backend/src/controllers/admin.ts
@@ -140,3 +140,116 @@ export const demoteAdminTouser = async (req: AuthRequest, res: Response) => {
 
   }
 }
+
+export const addInterestTag = async (req: AuthRequest, res: Response) => {
+  const { tag_name, description } = req.body;
+
+  if (!tag_name) {
+    return res.status(400).json({
+      message: "Tag name required"
+    });
+  }
+
+  const normalizedTag = tag_name.trim().toLowerCase();
+
+  try {
+
+    const existingTag = await pool.query(
+      `SELECT tag_id FROM interest_tag
+       WHERE LOWER(tag_name) = LOWER($1)`,
+      [normalizedTag]
+    );
+
+    if (existingTag.rowCount) {
+      return res.status(409).json({
+        message: "Tag already exists"
+      });
+    }
+
+    const tag = await pool.query(
+      `INSERT INTO interest_tag (tag_name, description)
+       VALUES ($1,$2)
+       RETURNING *`,
+      [normalizedTag, description || null]
+    );
+
+    return res.status(201).json(tag.rows[0]);
+
+  } catch (err) {
+
+    console.error("Create tag error:", err);
+
+    return res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+}
+
+export const deleteInterestTag = async (req: AuthRequest, res: Response) => {
+  const tagId = req.params.id;
+
+  if(!tagId){
+    return res.status(404).json({
+        message: "User id required"
+      });
+  }
+
+  try {
+
+    const tag = await pool.query(
+      `SELECT tag_id FROM interest_tag WHERE tag_id=$1`,
+      [tagId]
+    );
+
+    if (!tag.rowCount) {
+      return res.status(404).json({
+        message: "Tag not found"
+      });
+    }
+
+    await pool.query(
+      `DELETE FROM interest_tag WHERE tag_id=$1`,
+      [tagId]
+    );
+
+    return res.status(200).json({
+      message: "Tag deleted successfully"
+    });
+
+  } catch (err) {
+
+    console.error("Delete tag error:", err);
+
+    return res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+}
+
+export const getAllTags = async (req: AuthRequest, res: Response) => {
+    try {
+
+    const tags = await pool.query(
+      `SELECT
+         tag_id,
+         tag_name,
+         description,
+         created_at
+       FROM interest_tag
+       ORDER BY created_at DESC`
+    );
+
+    return res.status(200).json(tags.rows);
+
+  } catch (err) {
+
+    console.error("Fetch tags error:", err);
+
+    return res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+}
--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { verifyToken } from "../middleware/auth.js";
 import { isAdmin } from "../middleware/isAdmin.js";
-import { getAllUsers,promoteUserToAdmin,demoteAdminTouser } from "../controllers/admin.js";
+import { getAllUsers,promoteUserToAdmin,demoteAdminTouser,addInterestTag,deleteInterestTag,getAllTags } from "../controllers/admin.js";
 
 const router = Router();
 
 router.route("/users").get(verifyToken,isAdmin,getAllUsers);
 router.route("/promote/:id").put(verifyToken,isAdmin,promoteUserToAdmin);
 router.route("/demote/:id").put(verifyToken,isAdmin,demoteAdminTouser);
+router.route("/tags").post(verifyToken,isAdmin,addInterestTag);
+router.route("/tags/:id").delete(verifyToken,isAdmin,deleteInterestTag);
+router.route("/tags").get(verifyToken,isAdmin,getAllTags);
 
 export default router;
